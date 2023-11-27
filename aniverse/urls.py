b2c6e9954653303344bc0b134de4da0e40fdf068
Nickolaj <nikolajpetrishchev@gmail.com>
2023-11-28 00:00:32 +0600

    add swagger

--- aniverse/urls.py
+++ aniverse/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
@@ -7,6 +7,24 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Aniverse API",
+        default_version='v1',
+        description="Welcome to the Aniverse API, your gateway to a vibrant world of anime exploration and community engagement! Aniverse is a dynamic online resource crafted for anime enthusiasts seeking comprehensive information, interactive features, and a space to connect with fellow fans.",
+        terms_of_service="https://www.aniverse.site/terms/",
+        contact=openapi.Contact(email="contact@aniverse.site"),
+        license=openapi.License(name="MIT LICENSE"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +32,9 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('catalog_api/', include('anime_catalog.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
